c6c1a9c05882af41a615bf2bfba326996f063e58
Max Base <MaxBaseCode@Gmail.Com>
2019-02-09 19:43:12 +0000

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
+package main
 
+import (
+	"strconv"
+
+	"github.com/kataras/iris"
+
+	// "github.com/kataras/iris/middleware/logger"
+	// "github.com/kataras/iris/middleware/recover"
+
+	// "github.com/kataras/iris/mvc"
+
+	"github.com/kataras/iris/sessions"
+	"github.com/gorilla/securecookie"
+)
